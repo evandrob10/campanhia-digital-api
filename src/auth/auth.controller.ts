3e21fe40de579a273b48ca39cf344fc6135d78b7
Evandro Barros <evandrobarrosb98@gmail.com>
2025-05-15 00:00:05 -0300

    include credentials, alter name coockie return of jwt to token

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto/AuthDto';
 import { Response } from 'express';
@@ -8,9 +8,12 @@ export class AuthController {
     constructor(private readonly authService: AuthService) {}
 
     @Post()
-    authenticateUser(@Body() dataLogin: AuthDto, res: Response) {
-        const token = this.authService.authenticate(dataLogin);
-        res.cookie('jwt', token, {
+    async authenticateUser(
+        @Body() dataLogin: AuthDto,
+        @Res({ passthrough: true }) res: Response,
+    ) {
+        const token = await this.authService.authenticate(dataLogin);
+        res.cookie('token', token, {
             httpOnly: true,
             maxAge: 1000 * 60 * 60,
         });
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import cookieParser from 'cookie-parser';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.use(cookieParser());
-    app.enableCors();
+    app.enableCors({
+        origin: 'http://localhost:3000', // ou seu domínio do front
+        credentials: true, // ⬅️ OBRIGATÓRIO para permitir cookies
+    });
     await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
